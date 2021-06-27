ff186784ec1fe41ea56ecfef296ddd7ae92c7f9d
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-27 15:20:13 +0500

    Added routing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import {Container, Nav, Navbar} from 'react-bootstrap';
 
 const Header = (props) => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/card"><i className="fas fa-shopping-cart"/> Card</Nav.Link>
-                            <Nav.Link href="/signin"> <i className="fas fa-user"/> Sign in</Nav.Link>
+                            <LinkContainer to='/card'>
+                                <Nav.Link><i className="fas fa-shopping-cart"/> Card</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/signin'>
+                                <Nav.Link> <i className="fas fa-user"/> Sign in</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
