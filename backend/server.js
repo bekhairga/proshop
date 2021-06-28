62607bbec87e8bf2fdb49892d170dc86fedb08a8
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-28 09:09:23 +0500

    dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const products = require('./data/products');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send('api is running');
-})
-app.get('/api/products', (req, res)=>{
-    res.json(products);
-})
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(product => product._id === req.params.id);
-    res.json(product);
-})
-
-app.listen(5000, ()=>{
-    console.log("Work");
-})
+app.get('/', (req, res) => {
+	res.send('api is running');
+});
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((product) => product._id === req.params.id);
+	res.json(product);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`${process.env.NODE_ENV} mode running on port ${PORT}`);
+});
