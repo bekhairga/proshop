e9c7af6840a32bc86f8c40aae10ac0f3b1507114
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-28 11:38:02 +0500

    added custom error handler middleware for data that is not found or incorrect route

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+//middleware to handle non-existing routes
+const notFound = (req, res, next) => {
+	const error = new Error(`Not found - ${req.originalUrl}`);
+	res.status(404);
+	next(error);
+};
+//handling error, because it may send html
+const errorHandler = (err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+	res.status(statusCode).json({
+		message: err.message,
+		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+	});
+};
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 connectDB();
 const app = express();
+
 //middleware for /api/products routes
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
 	res.send('api is running');
