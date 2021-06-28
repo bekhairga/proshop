429f126ea14ed6bd5cc933dcea5cbb0003357347
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-28 13:22:52 +0500

    Implemented actions, reducers for product detail page

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 export const listProducts = () => async (dispatch) => {
 	try {
@@ -12,6 +15,21 @@ export const listProducts = () => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: PRODUCT_LIST_FAIL,
+			payload:
+				err.response && err.response.data.message
+					? err.response.data.message
+					: err.message,
+		});
+	}
+};
+export const listProductDetails = (id) => async (dispatch) => {
+	try {
+		dispatch({ type: PRODUCT_DETAILS_REQUEST });
+		const { data } = await axios(`/api/products/${id}`);
+		dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+	} catch (err) {
+		dispatch({
+			type: PRODUCT_DETAILS_FAIL,
 			payload:
 				err.response && err.response.data.message
 					? err.response.data.message
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
-const reducer = combineReducers({ productList: productListReducer });
+import {
+	productListReducer,
+	productDetailsReducer,
+} from './reducers/productReducers';
+const reducer = combineReducers({
+	productList: productListReducer,
+	productDetails: productDetailsReducer,
+});
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
