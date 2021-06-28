9ccea281e91ee6c1f6a244c66fd6afcf97478f3e
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-28 12:29:00 +0500

    Added redux, redux-thunk, react-redux. Created actions, constants, reducers and store

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,32 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import {Row, Col} from 'react-bootstrap';
-import Product from "../components/Product";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import axios from 'axios';
 
-const HomeScreen = props => {
-    const [products, setProducts] = useState([]);
-    useEffect(()=>{
-        const fetchProducts = async () =>{
-            const {data} = await axios('/api/products');
-            setProducts(data);
-        }
-        fetchProducts();
-    },[products])
-    return (
-        <>
-            <h1>Latest products</h1>
-            <Row>
-                {products.map(product => {
-                    return (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    )
-                })}
-            </Row>
-        </>
-    );
+const HomeScreen = (props) => {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios('/api/products');
+			setProducts(data);
+		};
+		try {
+			fetchProducts();
+		} catch (err) {
+			console.error(err.message);
+		}
+	}, [products]);
+	return (
+		<>
+			<h1>Latest products</h1>
+			<Row>
+				{products.map((product) => {
+					return (
+						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+						</Col>
+					);
+				})}
+			</Row>
+		</>
+	);
 };
-
 
 export default HomeScreen;
