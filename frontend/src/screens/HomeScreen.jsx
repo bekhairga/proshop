f8e4b13a9dc20591a8b82753ac150a434983f642
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-28 12:46:58 +0500

    Fetching all products from store, implementing loading and error. Also a way to not use HOC connect(), but only use useEffect from react and useDispatch, useSelector from react-redux

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,33 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 const HomeScreen = (props) => {
-	const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
 	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios('/api/products');
-			setProducts(data);
-		};
-		try {
-			fetchProducts();
-		} catch (err) {
-			console.error(err.message);
-		}
-	}, [products]);
+		dispatch(listProducts());
+	}, [dispatch]);
 	return (
 		<>
 			<h1>Latest products</h1>
-			<Row>
-				{products.map((product) => {
-					return (
-						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-							<Product product={product} />
-						</Col>
-					);
-				})}
-			</Row>
+			{loading ? (
+				<h2>Loading</h2>
+			) : error ? (
+				<h2>error</h2>
+			) : (
+				<Row>
+					{products.map((product) => {
+						return (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						);
+					})}
+				</Row>
+			)}
 		</>
 	);
 };
