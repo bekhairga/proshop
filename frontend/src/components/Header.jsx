e18179bc6a5444c133d12e480c9d1ed2b7c8664e
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-28 18:19:57 +0500

    Finished cart screen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
-import {LinkContainer} from 'react-router-bootstrap';
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = (props) => {
-    return (
-        <header>
-            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-                <Container>
-                    <LinkContainer to='/'>
-                        <Navbar.Brand href="/">ProShop</Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <LinkContainer to='/card'>
-                                <Nav.Link><i className="fas fa-shopping-cart"/> Card</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/signin'>
-                                <Nav.Link> <i className="fas fa-user"/> Sign in</Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
-}
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<LinkContainer to='/'>
+						<Navbar.Brand href='/'>ProShop</Navbar.Brand>
+					</LinkContainer>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ml-auto'>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									<i className='fas fa-shopping-cart' /> Card
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/signin'>
+								<Nav.Link>
+									{' '}
+									<i className='fas fa-user' /> Sign in
+								</Nav.Link>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
 
 export default Header;
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -24,7 +24,98 @@ const CartScreen = ({ match, location, history }) => {
 			dispatch(addToCart(productId, qty));
 		}
 	}, [dispatch, productId, qty]);
-	return <div>Cart</div>;
+	const removeCartHandler = (e) => {};
+	const checkoutHandler = () => {
+		history.push('/login?redirect=shipping');
+	};
+	return (
+		<Row>
+			<Col md={8}>
+				<h1>Shopping Cart</h1>
+				{cartItems.length === 0 ? (
+					<Message>
+						Your cart is empty <Link to='/'>Go Back</Link>
+					</Message>
+				) : (
+					<ListGroup variant='flush'>
+						{cartItems.map((item) => (
+							<ListGroup.Item key={item.product}>
+								<Row>
+									<Col md={2}>
+										<Image src={item.image} alt={item.name} fluid rounded />
+									</Col>
+									<Col md={3}>
+										<Link to={`/product/${item.product}`}>{item.name}</Link>
+									</Col>
+									<Col md={2}>{item.price}</Col>
+									<Col md={2}>
+										<Form.Control
+											onChange={(e) =>
+												dispatch(
+													addToCart(item.product, Number(e.target.value))
+												)
+											}
+											as='select'
+											value={item.quantity}
+										>
+											{[...Array(item.countInStock).keys()].map((x) => (
+												<option key={x + 1} value={x + 1}>
+													{x + 1}
+												</option>
+											))}
+										</Form.Control>
+									</Col>
+									<Col md={2}>
+										<Button
+											type='button'
+											variant='light'
+											onClick={(e) => removeCartHandler(e)}
+										>
+											<i className='fas fa-trash'></i>
+										</Button>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+				)}
+			</Col>
+			<Col md={4}>
+				<Card>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h2>
+								Subtotal (
+								{cartItems.reduce(
+									(accumulator, item) => accumulator + item.quantity,
+									0
+								)}
+								) items
+							</h2>
+							$
+							{cartItems
+								.reduce(
+									(accumulator, item) =>
+										accumulator + item.quantity * item.price,
+									0
+								)
+								.toFixed(2)}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Button
+								disabled={cartItems.length === 0}
+								type='button'
+								className='btn-block'
+								onClick={checkoutHandler}
+							>
+								Proceed to Checkout
+							</Button>
+						</ListGroup.Item>
+					</ListGroup>
+				</Card>
+			</Col>
+		</Row>
+	);
 };
 
 export default CartScreen;
