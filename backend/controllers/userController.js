1be0e981b11a7c96730d7bc4561ea302d787eba5
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-29 12:13:44 +0500

    example of getting token in userController.js from req.user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,20 +24,19 @@ const authUser = asyncHandler(async (req, res) => {
 //api/users/profile
 //private access
 const getUserProfile = asyncHandler(async (req, res) => {
-	// const user = await User.findById(req.user._id);
-	// if (user && (await user.matchPassword(password))) {
-	// 	res.json({
-	// 		_id: user._id,
-	// 		name: user.name,
-	// 		email: user.email,
-	// 		isAdmin: user.isAdmin,
-	// 		token: generateToken(user._id),
-	// 	});
-	// } else {
-	// 	res.status(401);
-	// 	throw new Error('Invalid email or password');
-	// }
-	res.send('Success');
+	const user = await User.findById(req.user._id);
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(401);
+		throw new Error('User not found');
+	}
 });
 
 export { authUser, getUserProfile };
