b51cd9dad5cca9b5653eba31611b62216c47513a
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-29 11:06:45 +0500

    Added user authentication

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import usersRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 
 //middleware for /api/products routes
 app.use('/api/products', productRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use(notFound);
 
