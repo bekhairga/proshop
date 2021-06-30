2b0a536720ccae722d138dda126beea6280ccd2d
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-30 12:06:35 +0500

    Added route and functionallity to update profile in backend usign put request

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,5 +64,29 @@ const registerUser = asyncHandler(async (req, res) => {
 		throw new Error('Invalid user data');
 	}
 });
+//update user profile
+//put
+//private route
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+		const updatedUser = await user.save();
+		res.status(201).json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	} else {
+		res.status(401);
+		throw new Error('User not found');
+	}
+});
 
-export { authUser, getUserProfile, registerUser };
+export { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,12 @@ import {
 	authUser,
 	getUserProfile,
 	registerUser,
+	updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
+router.route('/profile').put(protect, updateUserProfile);
 router.route('/').post(registerUser);
 export default router;
