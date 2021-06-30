c078acad1e75c6b7eb57ad27d25c169e5dd2c644
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-30 11:31:46 +0500

    Added functionality to show profile name if user logged in and also implemented user logout functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../actions/userActions';
 const Header = (props) => {
+	const dispatch = useDispatch();
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+	const logoutHandler = () => {
+		dispatch(logout());
+	};
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -18,12 +25,22 @@ const Header = (props) => {
 									<i className='fas fa-shopping-cart' /> Card
 								</Nav.Link>
 							</LinkContainer>
-							<LinkContainer to='/signin'>
-								<Nav.Link>
-									{' '}
-									<i className='fas fa-user' /> Sign in
-								</Nav.Link>
-							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id='username'>
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>
+										LogOut
+									</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to='/login'>
+									<Nav.Link>
+										<i className='fas fa-user' /> Sign in
+									</Nav.Link>
+								</LinkContainer>
+							)}
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
