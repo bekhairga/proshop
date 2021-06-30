75771d1ff649c4143e52fb96f0b2e2b85712ca43
BekhaIrga <irgashevbehzod@gmail.com>
2021-06-30 10:27:53 +0500

    Added functionality to register in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,4 +39,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, getUserProfile };
+//register new user
+//fetching all products
+//route: /api/users/login
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400);
+		throw new Error('User already exists');
+	}
+	const user = await User.create({ name, email, password });
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			password: user.password,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
+});
+
+export { authUser, getUserProfile, registerUser };
