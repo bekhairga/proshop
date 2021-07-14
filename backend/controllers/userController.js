e86ac2789b3bd0749d85dd542befe688de4e2fcb
BekhaIrga <irgashevbehzod@gmail.com>
2021-07-15 01:13:31 +0500

    Added middleware to check whether the user is admin or not and its protected route and added a route to fetch the list of all users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -89,4 +89,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+//get all users
+//api/users/
+//private access for admin
+const getUsers = asyncHandler(async (req, res) => {
+	const users = await User.find({});
+	res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 import {
 	authUser,
 	getUserProfile,
+	getUsers,
 	registerUser,
 	updateUserProfile,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { isAdmin, protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
 router.route('/profile').put(protect, updateUserProfile);
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
 export default router;
